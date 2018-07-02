510447ea18802434a9901e6aa5b22d5c73794c88
Chribtoper <xcstolfax@gmail.com>
2018-07-02 23:50:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  var myRanks = document.querySelectorAll('ul.ranked-list li');
+  for(let i = 0; i < myRanks.length; i++){
+  myRanks[i].innerHTML = (parseInt(myRanks[i].innerHTML)) + n;
+  }
+}
+
+function deepestChild(){
+  var mobDeep = document.getElementById('grand-node').querySelectorAll('div')
+  var x = mobDeep.length-1;
+  return mobDeep[x];
+}
